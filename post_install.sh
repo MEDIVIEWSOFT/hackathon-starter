4ed4544ec5714d60e3b5b59818c3cefcc2d9c9b2
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-27 17:21:20 +0900

    load environemntal variable from AWS SSM in dumb way

--- post_install.sh
+++ post_install.sh
@@ -5,6 +5,38 @@ set -e
 cd ~/ws2018-ticket
 npm install
 
+export MONGODB_URI=$(aws ssm get-parameters --region ap-northeast-2 --names MONGODB_URI --query Parameters[0].Value)
+export MONGOLAB_URI=$(aws ssm get-parameters --region ap-northeast-2 --names MONGOLAB_URI --query Parameters[0].Value)
+echo "export MONGODB_URI=$(aws ssm get-parameters --region ap-northeast-2 --names MONGODB_URI --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export MONGOLAB_URI=$(aws ssm get-parameters --region ap-northeast-2 --names MONGOLAB_URI --query Parameters[0].Value)" >> ~/.bash_profile
+
+export GOOGLE_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_ID --query Parameters[0].Value)
+export GOOGLE_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_SECRET --query Parameters[0].Value)
+echo "export GOOGLE_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_ID --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export GOOGLE_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_SECRET --query Parameters[0].Value)" >> ~/.bash_profile
+
+export LINKEDIN_ID=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_ID--query Parameters[0].Value)
+export LINKEDIN_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_SECRET --query Parameters[0].Value)
+export LINKEDIN_CALLBACK_URL=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_CALLBACK_URL --query Parameters[0].Value)
+
+echo "export LINKEDIN_ID=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_ID--query Parameters[0].Value)" >> ~/.bash_profile
+echo "export LINKEDIN_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_SECRET --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export LINKEDIN_CALLBACK_URL=$(aws ssm get-parameters --region ap-northeast-2 --names LINKEDIN_CALLBACK_URL --query Parameters[0].Value)" >> ~/.bash_profile
+
+export MAILGUN_ADDR=$(aws ssm get-parameters --region ap-northeast-2 --names MAILGUN_ADDR --query Parameters[0].Value)
+export MAILGUN_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names MAILGUN_PASSWORD --query Parameters[0].Value)
+
+echo "export MAILGUN_ADDR=$(aws ssm get-parameters --region ap-northeast-2 --names MAILGUN_ADDR --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export MAILGUN_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names MAILGUN_PASSWORD --query Parameters[0].Value)" >> ~/.bash_profile
+
+export IMP_AMOUNT=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_AMOUNT --query Parameters[0].Value)
+export IMP_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_KEY --query Parameters[0].Value)
+export IMP_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_SECRET --query Parameters[0].Value)
+
+echo "export IMP_AMOUNT=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_AMOUNT --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export IMP_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_KEY --query Parameters[0].Value)" >> ~/.bash_profile
+echo "export IMP_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names IMP_SECRET --query Parameters[0].Value)" >> ~/.bash_profile
+
 # setup NODE_ENV
 if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
     export NODE_ENV=$DEPLOYMENT_GROUP_NAME
