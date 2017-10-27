0640796f1b416bdd9d8a88844a2b287fafae9605
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-27 17:53:51 +0900

    fix validate.sh

--- deploy_scripts/validate.sh
+++ deploy_scripts/validate.sh
@@ -2,4 +2,5 @@
 sleep 10
 
 export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value)
-nc -zv 127.0.0.1 $((10#$PORT + 1))
+export PORT_=$((10#$PORT + 0))
+nc -zv 127.0.0.1 $PORT_
