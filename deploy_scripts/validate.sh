885cffa16e9fbb1ea68127e848668c85211a2dad
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-30 07:52:11 +0900

    fix wrong port string manipulation

--- deploy_scripts/validate.sh
+++ deploy_scripts/validate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sleep 10
 
-export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value)
-export PORT_=$((10#$PORT + 0))
-nc -zv 127.0.0.1 $PORT_
+port_=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value)
+port=$(sed -e 's/^"//' -e 's/"$//' <<<"$port_")
+
+nc -zv 127.0.0.1 $port
