101d7b381bd54de5145e40c4a1a250c4f777afd1
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-30 05:21:44 +0900

    clean scripts

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -4,22 +4,22 @@ set -e
 # update instance
 yum -y update
 
-# install g++ make
-yum install -y gcc-c++ make
-
-# install ssm-env
-#yum install -y golang
-#export GOPATH=$HOME/go
-#export PATH=$PATH:${GOPATH//://bin:}/bin
-
-# load environmental variable
-# go get -u "github.com/remind101/ssm-env"
-# ssm-env env
-# echo "ssm-env env" >> ~/.bash_profile
+# install CodeDeploy
+export AWS_REGION=ap-northeast-2
+cd /home/ec2-user
+yum install -y ruby aws-cli
+aws s3 cp s3://aws-codedeploy-$AWS_REGION/latest/install . --region $AWS_REGION
+chmod +x ./install
+./install auto
+rm -f install
 
 # add nodejs to yum
 curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo -E bash -
-yum -y install nodejs
+yum -y install make gcc gcc-c++ nodejs default-jre ImageMagick
+
+# install latest stable node version with "n"
+npm install -g n
+n stable
 
 export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value)
 echo "export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value)" >> ~/.bash_profile
