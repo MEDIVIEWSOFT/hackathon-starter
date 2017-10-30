37d4af52b27b2d84819a2aa8673327800372c6f9
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-30 16:02:26 +0900

    set true proxy for secure cookie and set session resave to false (recommended default)

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('trust proxy', 1);
 app.use(expressStatusMonitor());
 app.use(compression());
 app.use(sass({
@@ -83,7 +84,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(session({
-  resave: true,
+  resave: false,
   saveUninitialized: true,
   genid: function(req) {
     return uuidv4(); // use UUIDs for session IDs
