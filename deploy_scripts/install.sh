9af7dc1d0fec3f6acd44c7d68b4d55eeea1dce69
Liam Jongsu Kim <appleparan@gmail.com>
2017-10-30 10:18:26 +0900

    fix mistypo

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -55,7 +55,7 @@ server {
                 ;
 
     location / {
-        proxy_pass http://localhost:$PORT_;
+        proxy_pass http://localhost:$PORT;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
